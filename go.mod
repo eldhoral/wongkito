d91f640d0a53f5cfa6b54b6f695e3a3062dde53c
eldhoral123 <eldho@samudrainovasi.co.id>
2024-01-05 15:51:15 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module go_restapi
+
+go 1.19
+
+require (
+	github.com/golang-module/carbon/v2 v2.2.3
+	github.com/gorilla/mux v1.8.0
+	github.com/spf13/cast v1.5.1
+)
+
+require (
+	github.com/Rhymen/go-whatsapp v0.1.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/golang/protobuf v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
--- main.go
+++ main.go
@@ -1,110 +1,379 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
 	"encoding/json"
-	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
-
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/golang-module/carbon/v2"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/cast"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-// Book struct (Model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+type Order struct {
+	Username     string `json:"username"`
+	BuyerSkuCode string `json:"buyer_sku_code"`
+	CustomerNo   string `json:"customer_no"`
+	RefId        string `json:"ref_id"`
+	Sign         string `json:"sign"`
+	PrivateKey   string `json:"private_key"`
 }
 
-// Author struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+type Cek struct {
+	Username     string `json:"username"`
+	BuyerSkuCode string `json:"buyer_sku_code"`
+	CustomerNo   string `json:"customer_no"`
+	RefId        string `json:"ref_id"`
+	Sign         string `json:"sign"`
 }
 
-// Init books var as a slice Book struct
-var books []Book
+var orders []Order
 
-// Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+type PesananItemkuRequest struct {
+	DateStart   string `json:"date_start"`
+	OrderStatus string `json:"order_status"`
+}
+
+type PesananItemkuResponse struct {
+	Success bool `json:"success"`
+	Data    []struct {
+		OrderID             int         `json:"order_id"`
+		OrderNumber         string      `json:"order_number"`
+		ProductID           int         `json:"product_id"`
+		Price               int         `json:"price"`
+		Quantity            int         `json:"quantity"`
+		GameName            string      `json:"game_name"`
+		ProductName         string      `json:"product_name"`
+		UsingDeliveryInfo   int         `json:"using_delivery_info"`
+		DeliveryInfoField   interface{} `json:"delivery_info_field"`
+		Status              string      `json:"status,omitempty"`
+		RequiredInformation string      `json:"required_information"`
+		DeliveryInfo        interface{} `json:"delivery_info"`
+		OrderIncome         int         `json:"order_income"`
+	} `json:"data"`
+	Message    string `json:"message"`
+	StatusCode string `json:"statusCode"`
+}
+
+type ResponseDigiflazz struct {
+	Data struct {
+		RefID          string `json:"ref_id"`
+		CustomerNo     string `json:"customer_no"`
+		BuyerSkuCode   string `json:"buyer_sku_code"`
+		Message        string `json:"message"`
+		Status         string `json:"status"`
+		Rc             string `json:"rc"`
+		BuyerLastSaldo int    `json:"buyer_last_saldo"`
+		Sn             string `json:"sn"`
+		Price          int    `json:"price"`
+		Tele           string `json:"tele"`
+		Wa             string `json:"wa"`
+	} `json:"data"`
+}
+
+type ErrorResponse struct {
+	Status     int         `json:"status"`
+	Message    string      `json:"message"`
+	ResponseDg interface{} `json:"response_digi_flazz"`
+}
+
+func orderManual(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+	var order Order
+	_ = json.NewDecoder(r.Body).Decode(&order)
+	order.Username = "hubugeo6zxQo"
+	order.RefId = "manual-" + order.BuyerSkuCode + "-" + carbon.Now().ToDateMicroString()
+	sign := "hubugeo6zxQoa2c8d3e0-1960-5653-8fc1-669f53cca959" + order.RefId
+	data := []byte(sign)
+	order.Sign = fmt.Sprintf("%x", md5.Sum(data))
+	json.NewEncoder(w).Encode(&order)
 }
 
-// Get single book
-func getBook(w http.ResponseWriter, r *http.Request) {
+func pembayaran(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Gets params
-	// Loop through books and find one with the id from the params
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+	var order Order
+	_ = json.NewDecoder(r.Body).Decode(&order)
+	var decodedByte, _ = base64.StdEncoding.DecodeString(order.PrivateKey)
+	var decodedString = string(decodedByte)
+	if decodedString != "wongkitostore@gmail.com" {
+		var errResponse ErrorResponse
+		errResponse.Status = http.StatusForbidden
+		errResponse.Message = "Failed to process API"
+		errResponse.ResponseDg = ResponseDigiflazz{}
+		json.NewEncoder(w).Encode(&errResponse)
+		return
+	}
+	order.Username = "hubugeo6zxQo"
+	order.RefId = "manual-" + order.BuyerSkuCode + "-" + carbon.Now().ToDateMicroString()
+	sign := "hubugeo6zxQoa2c8d3e0-1960-5653-8fc1-669f53cca959" + order.RefId
+	data := []byte(sign)
+	order.Sign = fmt.Sprintf("%x", md5.Sum(data))
+	// foo_marshalled, err := json.Marshal(order)
+	// fmt.Fprint(w, string(foo_marshalled)) // write response to ResponseWriter (w)
+	code, response, err := HitDigiflazz(order)
+	if err != nil {
+		var errResponse ErrorResponse
+		errResponse.Status = code
+		errResponse.Message = err.Error()
+		errResponse.ResponseDg = response
+		json.NewEncoder(w).Encode(&errResponse)
+		return
+	}
+	if code != 200 {
+		var errResponse ErrorResponse
+		errResponse.Status = code
+		errResponse.Message = response.Data.Message
+		errResponse.ResponseDg = response
+		json.NewEncoder(w).Encode(&errResponse)
+		return
+	}
+	orders = append(orders, order)
+	json.NewEncoder(w).Encode(&response)
+}
+
+func cekTagihan(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var cek Cek
+	_ = json.NewDecoder(r.Body).Decode(&cek)
+	for _, field := range orders {
+		if field.RefId == cek.RefId {
+			var order Order
+			order.Username = field.Username
+			order.BuyerSkuCode = field.BuyerSkuCode
+			order.CustomerNo = field.CustomerNo
+			order.RefId = field.RefId
+			order.Sign = field.Sign
+			code, response, err := HitDigiflazz(order)
+			if err != nil {
+				fmt.Print(err.Error())
+				var errResponse ErrorResponse
+				errResponse.Status = code
+				errResponse.Message = err.Error()
+				errResponse.ResponseDg = response
+				json.NewEncoder(w).Encode(&errResponse)
+				return
+			}
+			if code != 200 {
+				var errResponse ErrorResponse
+				errResponse.Status = code
+				errResponse.Message = response.Data.Message
+				errResponse.ResponseDg = response
+				json.NewEncoder(w).Encode(&errResponse)
+				return
+			}
+			json.NewEncoder(w).Encode(&response)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
+
 }
 
-// Add new book
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
+func HitDigiflazz(order Order) (httpStatus int, dg ResponseDigiflazz, err error) {
+	fmt.Println("Calling API...")
 
-// Update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
+	reqPartnerValidity, err := json.Marshal(order)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
 	}
+	bodyJson := string(reqPartnerValidity)
+	var ioreader io.Reader
+	if bodyJson != "" {
+		ioreader = bytes.NewBuffer([]byte(bodyJson))
+	}
+	req, err := http.NewRequest("POST", "https://api.digiflazz.com/v1/transaction", ioreader)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	var responseObject ResponseDigiflazz
+	json.Unmarshal(bodyBytes, &responseObject)
+	fmt.Printf("API Response as struct %+v\n", responseObject)
+	dg = responseObject
+	return resp.StatusCode, dg, err
 }
 
-// Delete book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
+func cekPesananItemku(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
+	privateKey := r.Header.Get("private_key")
+	var decodedByte, _ = base64.StdEncoding.DecodeString(privateKey)
+	var decodedString = string(decodedByte)
+	if decodedString != "wongkitostore@gmail.com" {
+		var errResponse ErrorResponse
+		errResponse.Status = http.StatusForbidden
+		errResponse.Message = "Failed to process API"
+		errResponse.ResponseDg = ResponseDigiflazz{}
+		json.NewEncoder(w).Encode(&errResponse)
+		return
 	}
-	json.NewEncoder(w).Encode(books)
+
+	requestPesanan := PesananItemkuRequest{DateStart: carbon.Yesterday().ToDateString(), OrderStatus: "REQUIRE_PROCESS"}
+	code, response, err := hitItemkuOrderList(requestPesanan)
+	if err != nil {
+		fmt.Print(err.Error())
+		var errResponse ErrorResponse
+		errResponse.Status = code
+		errResponse.Message = err.Error()
+		errResponse.ResponseDg = response
+		json.NewEncoder(w).Encode(&errResponse)
+		return
+	}
+	if code != 200 {
+		var errResponse ErrorResponse
+		errResponse.Status = code
+		errResponse.Message = err.Error()
+		errResponse.ResponseDg = response
+		json.NewEncoder(w).Encode(&errResponse)
+		return
+	}
+	if response.Success == true {
+		numberOfOrder := len(response.Data)
+		url := "https://api.callmebot.com/whatsapp.php?phone=6281291755506&text=Ada+" + cast.ToString(numberOfOrder) + "+pesanan+baru+itemku&apikey=" + os.Getenv("API_KEY_WHATSAPP")
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		defer resp.Body.Close()
+	}
+	json.NewEncoder(w).Encode(&response)
+	return
+
+}
+
+func hitItemkuOrderList(requestItemku PesananItemkuRequest) (httpStatus int, respPI PesananItemkuResponse, err error) {
+	nonce := time.Now()
+
+	resultMarshal, err := json.Marshal(requestItemku)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	bodyJson := string(resultMarshal)
+	var ioreader io.Reader
+	if bodyJson != "" {
+		ioreader = bytes.NewBuffer([]byte(bodyJson))
+	}
+
+	tokenBearer, err := generateJwtItemku("VFF_JLdc3Wnk5shcO3Du", cast.ToString(nonce.Unix()), requestItemku)
+	fmt.Println(tokenBearer)
+	req, err := http.NewRequest("POST", "https://tokoku-gateway.itemku.com/api/order/list", ioreader)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Api-Key", "VFF_JLdc3Wnk5shcO3Du")
+	req.Header.Set("Nonce", cast.ToString(nonce.Unix()))
+	req.Header.Set("Authorization", "Bearer "+tokenBearer)
+	fmt.Println(req.Header)
+	fmt.Println(req.Body)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	var responseObject PesananItemkuResponse
+	json.Unmarshal(bodyBytes, &responseObject)
+	fmt.Printf("API Response as struct %+v\n", responseObject)
+	respPI = responseObject
+	return resp.StatusCode, respPI, err
+}
+
+func generateJwtItemku(xApiKey, Nonce string, requestBody PesananItemkuRequest) (tokenString string, err error) {
+	tokenJwt := jwt.Token{
+		Header: map[string]interface{}{
+			"X-Api-Key": xApiKey,
+			"Nonce":     Nonce,
+			"alg":       "HS256",
+		},
+		Claims: jwt.MapClaims{
+			"date_start":   requestBody.DateStart,
+			"order_status": requestBody.OrderStatus,
+		},
+		Method: jwt.SigningMethodHS256,
+	}
+	var secretKey = []byte("q0lo9uaXexfLZiQBkLO6iGh1G7gI-BjPrdSZeBoZ")
+	tokenString, err = tokenJwt.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
 
 // Main function
 func main() {
+	fmt.Println("mulai")
+
+	// start assigning config url
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error load .env : %v", err)
+		panic(err)
+	}
+	var configUrl ConfigEnvironment
+	err = envconfig.Process("", &configUrl)
+	if err != nil {
+		fmt.Printf("Error assigning configUrl from .env : %v", err)
+		panic(err)
+	}
+
 	// Init router
 	r := mux.NewRouter()
 
-	// Hardcoded data - @todo: add database
-	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+	r.HandleFunc("/digiflazz/pembayaran", pembayaran).Methods("POST")
+	r.HandleFunc("/digiflazz/cek", cekTagihan).Methods("POST")
+	r.HandleFunc("/digiflazz/manual-pembayaran", orderManual).Methods("POST")
 
-	// Route handles & endpoints
-	r.HandleFunc("/books", getBooks).Methods("GET")
-	r.HandleFunc("/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/books", createBook).Methods("POST")
-	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/itemku/pesanan/cek", cekPesananItemku).Methods("POST")
 
+	fmt.Println("API jalan")
 	// Start server
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8090", r))
+
+}
+
+type ConfigEnvironment struct {
+	Whatsapp Whatsapp
+}
+type Whatsapp struct {
+	Key string `envconfig:"API_KEY_WHATSAPP" required:"true"`
 }
 
 // Request sample
